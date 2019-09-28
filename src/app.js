f08f09a0555a34ec2fafb2d46afe49ac03827c6c
Сидоров Константин <sidorovalpha@gmail.com>
2019-09-29 01:53:14 +0300

    changed routes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
 import React from "react";
 import axios from "axios";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { Menu } from "./components/organisms/menu";
+import { Documentation } from "./components/organisms/documentation";
 import ListWithParamsPaginationContainer from "./example/containers/list-with-params-pagination-container";
 import ListWithoutParamsPaginationContainer from "./example/containers/list-without-params-pagination-container";
-import { Documentation } from "./components/organisms/documentation";
 
 axios.defaults.baseURL = "https://swapi.co/api";
 
 export const App = () => (
   <Menu>
     <Switch>
-      <Route exact path="/" component={Documentation} />
-      <Route path="/params" component={ListWithParamsPaginationContainer} />
-      <Route
-        path="/only-page-param"
-        component={ListWithoutParamsPaginationContainer}
-      />
+      <Redirect exact from="/" to="/documentation" />
+      <Route path="/documentation">
+        <Documentation />
+      </Route>
+      <Route path="/pagination-with-params">
+        <ListWithParamsPaginationContainer />
+      </Route>
+      <Route path="/pagination-without-params">
+        <ListWithoutParamsPaginationContainer />
+      </Route>
     </Switch>
   </Menu>
 );
